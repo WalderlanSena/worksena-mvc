2c8b3bbeb35aa11c098f2d4af34d13fc1199bfaf
Walderlan Sena <senawalderlan@gmail.com>
2018-09-15 20:22:35 -0300

    feature: Adicionado a primeira versão instavel do componente worksena-mvs

--- src/AbstractActionController.php
+++ src/AbstractActionController.php
@@ -12,16 +12,65 @@ namespace MVS\WorksenaMvc;
 
 abstract class AbstractActionController
 {
-    private $layout;
     private $action;
+    protected $pageTitle;
+    protected $pageDescription;
 
-    protected function render($view, $layout = true, $data = [])
+    protected function render($view, $layout = true, $data)
     {
+        $this->action = $view;
 
+        if ($layout == true && file_exists(__DIR__ . '/../../../../templates/ws-default/layout.php')) {
+            return include_once __DIR__ . '/../../../../templates/ws-default/layout.php';
+        }
+
+        return $this->content($data);
     }
 
-    private function content()
+    private function content($data)
     {
+        $current = get_class($this);
+        $moduleName = explode('\\', $current);
+        $actionLocation = str_replace('.', '/', $this->action);
+        return include_once __DIR__ . '/../../../../module/'.$moduleName[0].'/view/'.strtolower($actionLocation).'.phtml';
+    }
+
+    /**
+     * Configurando titulo dinâmico para as views
+     * @param $pageTitle
+     */
+    protected function setPageTitle($pageTitle)
+    {
+        $this->pageTitle = $pageTitle;
+    }
+
+    /**
+     * Adiciona separador no titulo da página
+     * @param null $separator
+     * @return null|string
+     */
+    protected function getPageTitle($separator = null)
+    {
+        if ($separator) {
+            return $this->pageTitle . " " . $separator . " ";
+        }
+        return $this->pageTitle;
+    }//end function getPageTitle
+
+    /**
+     * @param $description
+     */
+    protected function setDescription($description)
+    {
+        $this->pageDescription = $description;
+    }
+
+    /**
+     * @return null
+     */
+    protected function getDescription()
+    {
+        return $this->pageDescription;
 
     }
 }
